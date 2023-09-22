ae19e5b948773a5539dd6a7f61af63d9fa40e831
Mohammed Kamil <kamlan19922@gmail.com>
2023-09-22 20:35:03 +0400

    init

---
+++ src/test/java/io/mohadkamil/jdbc/JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mohadkamil.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
